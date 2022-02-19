e91534f0358a6a8ef289f73da4c9c3371956eae0
andnix <55647565+andnix@users.noreply.github.com>
2022-02-19 10:59:16 +0100

    Update install_first.sh

--- install_first.sh
+++ install_first.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BTRFS="/dev/nvme1n1p2"
+
 loadkeys sv-latin1
 pacman -Syy
 timedatectl set-ntp true
@@ -12,20 +14,21 @@ mount /dev/mapper/linux--vg-arch /mnt
 # Creating BTRFS subvolumes.
 print "Creating BTRFS subvolumes."
 
-#for volume in @ @home @root @opt @srv @snapshots @var
+#for volume in @ @home @root @opt @srv @snapshots @var @swap
 #do
 #    btrfs su cr /mnt/$volume
 #done
 
 #umount /mnt
 #mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@ /dev/mapper/linux--vg-arch /mnt
-#mkdir -p /mnt/{boot,home,root,opt,srv,.snapshots,var}
+#mkdir -p /mnt/{boot,home,root,opt,srv,.snapshots,var,swap}
 #mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@home /dev/mapper/linux--vg-arch /mnt/home
 #mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@root /dev/mapper/linux--vg-arch /mnt/root
 #mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@opt /dev/mapper/linux--vg-arch /mnt/opt
 #mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@srv /dev/mapper/linux--vg-arch /mnt/srv
 #mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@snapshots /dev/mapper/linux--vg-arch /mnt/.snapshots
 #mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@var /dev/mapper/linux--vg-arch /mnt/var
+#mount -o defaults,noatime,subvol=@swap /dev/mapper/linux--vg-arch /mnt/swap
 #chattr +C /mnt/var
 
 #btrfs subvolume create /mnt/@
