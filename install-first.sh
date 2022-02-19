736d5bdcc9a0d581873561ac3d37d7fdbbbf7b11
andnix <55647565+andnix@users.noreply.github.com>
2022-02-19 17:00:13 +0100

    Update install-first.sh

--- install-first.sh
+++ install-first.sh
@@ -3,6 +3,9 @@
 EFI="/dev/nvme1n1p1"
 BTRFS="/dev/nvme1n1p2"
 
+username="andreas"
+userpass="password"
+
 loadkeys sv-latin1
 pacman -Syy
 timedatectl set-ntp true
@@ -74,3 +77,55 @@ arch-chroot /mnt
 git clone https://github.com/andnix/arch_install.git
 chmod +x /arch_install/install-as-root.sh
 chmod +x /arch_install/install-as-user.sh
+
+#timedatectl set-ntp true
+ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
+hwclock --systohc
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8' | tee -a /etc/locale.conf > /dev/null
+echo 'KEYMAP=sv-latin1' | tee -a /etc/vconsole.conf > /dev/null
+localectl set-x11-keymap se
+echo 'arch' | tee -a /etc/hostname > /dev/null
+echo '127.0.0.1	localhost\n::1		localhost\n127.0.1.1	arch.localdomain	arch' | tee -a /etc/hosts > /dev/null
+echo 'root:password' | chpasswd
+
+pacman -S  alsa-utils base-devel efibootmgr firewalld grub grub-btrfs gvfs lvm2 networkmanager bluez bluez-utils os-prober pacman-contrib pulseaudio rsync snap-pac snapper ttf-font-awesome ttf-roboto udiskie
+pacman -S --noconfirm nvidia nvidia-settings
+
+# Modules for BTRFS and NVIDIA
+sed -i 's/^MODULES=.*/MODULES=\(btrfs nvidia nvidia_modeset nvidia_uvm nvidia_drm\)/' /etc/mkinitcpio.conf
+
+# If using LVM
+#sed -i 's/\(^HOOKS.*block \)\(filesystems.*\)/\1lvm2 \2/' /etc/mkinitcpio.conf
+
+mkinitcpio -P
+
+# Setting up GRUB
+sed -i 's/^GRUB_GFXMODE=.*/GRUB_GFXMODE=3440x1440x32/' /etc/default/grub
+sed -i 's/^GRUB_DEFAULT=.*/GRUB_DEFAULT=saved/' /etc/default/grub
+sed -i 's/\(^GRUB_CMDLINE_LINUX_DEFAULT=".*\)\(.\)$/\1 nvidia-drm.modeset=1\2/' /etc/default/grub
+sed -i 's/^#GRUB_SAVEDEFAULT=.*/GRUB_SAVEDEFAULT=true/' /etc/default/grub
+echo 'GRUB_DISABLE_OS_PROBER=false' | tee -a /etc/default/grub > /dev/null
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Enable Services
+systemctl enable NetworkManager
+systemctl enable fstrim.timer
+systemctl enable bluetooth
+systemctl enable firewalld
+systemctl enable systemd-timesyncd
+
+# Fix Keychron Bluetooth Keyboard Connection
+sed -i 's/#AutoEnable=false/AutoEnable=true/' /etc/bluetooth/main.conf
+sed -i 's/#FastConnectable.*/FastConnectable = true/' /etc/bluetooth/main.conf
+sed -i 's/#\(ReconnectAttempts=.*\)/\1/' /etc/bluetooth/main.conf
+sed -i 's/#\(ReconnectIntervals=.*\)/\1/' /etc/bluetooth/main.conf
+
+useradd -m andreas
+echo "$username:$userpass" | chpasswd
+echo "$username ALL=(ALL) ALL" | tee -a /etc/sudoers.d/andreas > /dev/null
+
+printf "Exit, umount -a, reboot.\nAfter reboot login as normal user and run install-as-user.sh"
