09ba5a86202bcd777328819f9be57e763815f5ed
andnix <55647565+andnix@users.noreply.github.com>
2022-02-19 07:19:29 +0100

    Update install_first.sh

--- install_first.sh
+++ install_first.sh
@@ -8,15 +8,12 @@ mount /dev/mapper/linux--vg-arch /mnt
 
 # Creating BTRFS subvolumes.
 print "Creating BTRFS subvolumes."
-for volume in @ @home @root @opt @srv @snapshots @var_log @pkg
-do
-    btrfs su cr /mnt/$volume
-done
 
 #for volume in @ @home @root @opt @srv @snapshots @var
 #do
 #    btrfs su cr /mnt/$volume
 #done
+
 #umount /mnt
 #mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@ /dev/mapper/linux--vg-arch /mnt
 #mkdir -p /mnt/{boot,home,root,opt,srv,.snapshots,var}
@@ -33,10 +30,15 @@ done
 #btrfs subvolume create /mnt/@snapshots
 #btrfs subvolume create /mnt/@var_log
 #btrfs subvolume create /mnt/@pkg
+
+for volume in @ @home @root @opt @srv @snapshots @var_log @pkg
+do
+    btrfs su cr /mnt/$volume
+done
+
 umount /mnt
 mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@ /dev/mapper/linux--vg-arch /mnt
 mkdir -p /mnt/{boot,home,root,opt,srv,.snapshots,var/log,var/cache/pacman/pkg}
-#mkdir -p /mnt/{boot,home,root,opt,srv,.snapshots,var}
 mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@home /dev/mapper/linux--vg-arch /mnt/home
 mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@root /dev/mapper/linux--vg-arch /mnt/root
 mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@opt /dev/mapper/linux--vg-arch /mnt/opt
@@ -44,9 +46,8 @@ mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@srv /dev/m
 mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@snapshots /dev/mapper/linux--vg-arch /mnt/.snapshots
 mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@var_log /dev/mapper/linux--vg-arch /mnt/var/log
 mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@pkg /dev/mapper/linux--vg-arch /mnt/var/cache/pacman/pkg
-#mount -o noatime,compress=zstd:1,space_cache=v2,discard=async,subvol=@var /dev/mapper/linux--vg-arch /mnt/var
-#chattr +C /mnt/var
 chattr +C /mnt/var/log
+
 mount /dev/nvme1n1p1 /mnt/boot
 swapon /dev/mapper/linux--vg-swap
 pacstrap /mnt base linux linux-firmware amd-ucode btrfs-progs git nano
@@ -54,7 +55,5 @@ genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 
 git clone https://github.com/andnix/arch_install.git
-nano /arch_install/install.uefi (change password for root and user)
 chmod +x /arch_install/install-uefi.sh
 chmod +x /arch_install/install-bspwm.sh
-./arch_install/install_uefi.sh
