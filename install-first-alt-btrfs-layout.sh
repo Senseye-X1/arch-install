d3e5cf67c99aa25e2751343d41e93dbab3ae9258
Senseye-X1 <55647565+Senseye-X1@users.noreply.github.com>
2022-02-25 17:50:55 +0100

    Update install-first-alt-btrfs-layout.sh

--- install-first-alt-btrfs-layout.sh
+++ install-first-alt-btrfs-layout.sh
@@ -304,7 +304,7 @@ arch-chroot /mnt /bin/bash -e <<EOF
 
     # Installing GRUB.
     echo "Installing GRUB on /boot."
-    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB &>/dev/null
+    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB &>/dev/null
     
     # Creating grub config file.
     echo "Creating GRUB config file."
