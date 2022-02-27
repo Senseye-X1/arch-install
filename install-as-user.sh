fe984da44d17c4a3bffc505956b424ecd0666334
Senseye-X1 <55647565+Senseye-X1@users.noreply.github.com>
2022-02-27 21:25:52 +0100

    Update install-as-user.sh

--- install-as-user.sh
+++ install-as-user.sh
@@ -30,12 +30,6 @@ cd
 paru polybar
 
 sudo systemctl enable lightdm
-#sudo systemctl enable --now snapper-timeline.timer
-#sudo systemctl enable --now snapper-cleanup.timer
-#sudo systemctl enable grub-btrfs.path
-
-#sudo mkdir /etc/pacman.d/hooks
-#echo '[Trigger]\nOperation = Upgrade\nOperation = Install\nOperation = Remove\nType = Path\nTarget = usr/lib/modules/*/vmlinuz\n\n[Action]\nDepends = rsync\nDescription = Backing up /boot...\nWhen = PreTransaction\nExec = /usr/bin/rsync -a --delete /boot /.bootbackup' | sudo tee -a /etc/pacman.d/hooks/50-bootbackup.hook > /dev/null
 
 # Use this if you need keyboard numlock enabled at boot.
 # Remember to install package numlockx with: pacman -S numlockx
@@ -51,27 +45,26 @@ sudo systemctl enable lightdm
 #bluetoothctl trust DC:2C:26:FF:17:17
 
 # Fetching .configs from git
-git clone https://github.com/andnix/arch_install.git
-#chmod +x /arch_install/install-as-root.sh
+#chmod +x /arch_install/install-as-user.sh
 
-print "Populating .configs."
-mkdir ~/.config/{bspwm,dunst,gtk-3.0,kitty,picom,polybar,rofi,sxhkd}
-install -m644 /arch_install/.zshrc ~/.zshrc
-echo 'setxkbmap se\nxrdb ~/.Xresources' | tee -a ~/.xprofile > /dev/null
-echo 'Xcursor.theme: Adwaita\nXcursor.size: 16' | tee -a ~/.Xresources > /dev/null
+#print "Populating .configs."
+#mkdir ~/.config/{bspwm,dunst,gtk-3.0,kitty,picom,polybar,rofi,sxhkd}
+#install -m644 /arch_install/.zshrc ~/.zshrc
+#echo 'setxkbmap se\nxrdb ~/.Xresources' | tee -a ~/.xprofile > /dev/null
+#echo 'Xcursor.theme: Adwaita\nXcursor.size: 16' | tee -a ~/.Xresources > /dev/null
 
-install -Dm755 /arch_install/.config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+#install -Dm755 /arch_install/.config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
 #echo "#! /bin/sh\n\n_() { bspc config \"\$@\";  }\n\nsxhkd &\npicom --config ~/.config/picom/picom.conf -b &\nsh ~/.config/polybar/launch.sh\nudiskie &\nlight-locker &\nxset s off\nxset dpms 0 0 0\nxautolock -time 15 -locker \"systemctl suspend\" -detectsleep &\n\nAC=\$(grep -m1 'ac = ' ~/.config/polybar/colors.ini | awk '{print \$3}')\n\nbspc wm --reorder-monitors DP-0 HDMI-0\nbspc monitor DP-0 -d 1 2\nbspc monitor HDMI-0 -d 3 4\n\n_ border_width 3\n_ window_gap 8\n_ focused_border_color \"\$AC\"\n_ normal_border_color '#171a1f'\n_ active_border_color '#171a1f'\n_ ignore_ewmh_focus true\n_ pointer_follows_monitor true\n\n_ split_ratio 0.52\n_ borderless_monocle true\n_ gapless_monocle true\n\n#bspc rule -a Gimp desktop='^5' state=floating follow=on\nbspc rule -a Google-chrome desktop='^1'\nbspc rule -a Geany desktop='^3'\nbspc rule -a firefox desktop='^1'" | tee ~/.config/bspwm/bspwmrc > /dev/null
 #chmod +x ~/.config/bspwm/bspwmrc
-install -Dm644 /arch_install/.config/dunst/dunstrc ~/.config/dunst/dunstrc
-install -Dm644 /arch_install/.config/gtk-3.0/* -t ~/.config/gtk-3.0
-install -Dm644 /arch_install/.config/kitty/* -t ~/.config/kitty
-install -Dm644 /arch_install/.config/picom/picom.conf ~/.config/picom/picom.conf
-install -Dm644 /arch_install/.config/polybar/* -t ~/.config/polybar
-chmod +x ~/.config/polybar/launch.sh
-install -Dm644 /arch_install/.config/rofi/* -t ~/.config/rofi
-install -Dm644 /arch_install/.config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
-install -Dm755 /arch_install/.scripts/* -t ~/.scripts
+#install -Dm644 /arch_install/.config/dunst/dunstrc ~/.config/dunst/dunstrc
+#install -Dm644 /arch_install/.config/gtk-3.0/* -t ~/.config/gtk-3.0
+#install -Dm644 /arch_install/.config/kitty/* -t ~/.config/kitty
+#install -Dm644 /arch_install/.config/picom/picom.conf ~/.config/picom/picom.conf
+#install -Dm644 /arch_install/.config/polybar/* -t ~/.config/polybar
+#chmod +x ~/.config/polybar/launch.sh
+#install -Dm644 /arch_install/.config/rofi/* -t ~/.config/rofi
+#install -Dm644 /arch_install/.config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+#install -Dm755 /arch_install/.scripts/* -t ~/.scripts
 #install -Dm644 /arch_install/.mozilla/firefox/a5qjmjc5.default-release/chrome/userChrome.css ~/.mozilla/firefox/*.default-release/chrome/userChrome.css
 
 print "All done."
